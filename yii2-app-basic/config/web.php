f5eb3883edbf6359625f6986506b86e936e1e9c2
Javier Campocosio <jcampocosio@gmail.com>
2024-01-29 10:12:19 +0100

    base de datos Conectada

--- yii2-app-basic/config/web.php
+++ yii2-app-basic/config/web.php
@@ -4,7 +4,9 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
-    'id' => 'basic',
+    'id' => 'BuilderLE',
+    'name' => 'Aplicacion de videojuegos',
+    'language' => 'es-ES',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
@@ -43,14 +45,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'showScriptName' => true,
             'rules' => [
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
