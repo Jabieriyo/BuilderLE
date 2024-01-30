e8c74786520f376a2422feb4388ccadc0d8eb679
Javier Campocosio <jcampocosio@gmail.com>
2024-01-30 17:56:09 +0100

    home por defecto eliminada y corousel creado

--- yii2-app-basic/views/layouts/main.php
+++ yii2-app-basic/views/layouts/main.php
@@ -81,8 +81,8 @@ AppAsset::register($this);
 
 <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
-        <p class="float-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="float-right"><?= Yii::powered() ?></p>
+        <p class="float-left">&copy; BuilderLE <?= date('Y') ?></p>
+        <p class="float-right">Desarrollado por Javier Campocosio Regato</p>
     </div>
 </footer>
 
--- yii2-app-basic/views/site/index.php
+++ yii2-app-basic/views/site/index.php
@@ -1,53 +1,62 @@
 <?php
-
+use yii\helpers\Html;
+use yii\helpers\Url;
 /** @var yii\web\View $this */
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Mi Proyecto Yii2</title>
 
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Crea y comparte tus mejores builds</h1>
+    <!-- Agrega las hojas de estilo de Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- Agrega tu propio archivo CSS si es necesario -->
 
-        <p class="lead">O copia las de otras personas</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    <!-- Incluye los scripts de Bootstrap y jQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- Agrega tus propios scripts si es necesario -->
+</head>
+<body>
+<!-- Carrusel de Bootstrap -->
+<div id="carouselhome" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+           <?= Html::img('@web/images/imagen1.jpeg', ['alt' => 'Evento Santander 21', 'class' => 'd-block w-100']) ?>
+        </div>
+        <div class="carousel-item">
+            <?= Html::img('@web/images/imagen2.jpg', ['alt' => 'Evento Santander 21', 'class' => 'd-block w-100']) ?>
+        </div>
+        <!-- Agrega más elementos según sea necesario -->
     </div>
+    <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+    </a>
+</div>
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+<!-- Div con texto -->
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">Bienvenido a Mi Proyecto Yii2</h2>
+                    <p class="card-text">Este es un ejemplo de una página de inicio con Yii2, Bootstrap, HTML y CSS.</p>
+                </div>
             </div>
         </div>
-
     </div>
 </div>
+
+</body>
+</html>
